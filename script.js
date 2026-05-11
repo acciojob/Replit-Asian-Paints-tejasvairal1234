22ca7bda507c7994a8ccab500e8fbb90b3ed5f23
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-05-11 11:55:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-//
-//your JS code here. If required.
 const changeBtn = document.getElementById("change_button");
-const resetBtn = document.getElementById("Reset");
+const resetBtn = document.getElementById("reset_button");
 
-// Function to reset all blocks
 function resetGrid() {
   const items = document.querySelectorAll(".grid-item");
 
@@ -12,21 +9,22 @@ function resetGrid() {
   });
 }
 
-// Change color
 changeBtn.addEventListener("click", () => {
+
   const blockId = document.getElementById("block_id").value;
+
   const color = document.getElementById("colour_id").value;
 
-  // Reset all first
   resetGrid();
 
-  // Change selected block color
   const block = document.getElementById(blockId);
 
   if (block) {
     block.style.backgroundColor = color;
   }
+
 });
 
-// Reset button
-resetBtn.addEventListener("click", resetGrid);
+resetBtn.addEventListener("click", () => {
+  resetGrid();
+});
